7ffbece9fb6e945352130c25f4d0706aeef9c775
Jonathan Zavala <jontzavala@gmail.com>
2020-11-18 12:53:19 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    i = 0
+    caller_location = []
+    while i < array.length
+        caller_location << yield(array[i])
+        i += 1
+    end
+    caller_location
+end
 
